4f9da9a330158b1d427402822bbd27f336944143
Leonardo Pereira <159678359+Leonarotti@users.noreply.github.com>
2025-05-27 19:51:28 -0600

    Update politicas.js

--- politicas.js
+++ politicas.js
@@ -1,5 +1,7 @@
-// Script para manejar el enlace de cierre
-document.querySelector('.btn-cerrar').addEventListener('click', function (event) {
-    event.preventDefault();
-    window.location.href = 'index.html#suscripcion'; // Navegar a la sección de suscripción
+document.addEventListener('DOMContentLoaded', function () {
+    // Script para manejar el enlace de cierre
+    document.querySelector('.btn-cerrar').addEventListener('click', function (event) {
+        event.preventDefault();
+        window.location.href = 'index.html#suscripcion'; // Navegar a la sección de suscripción
+    });
 });

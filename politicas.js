b6cf8fc799f799411254afe98d61c8a943201f41
Leonardo Pereira <159678359+Leonarotti@users.noreply.github.com>
2025-05-27 19:49:04 -0600

    Create politicas.js

---
+++ politicas.js
@@ -0,0 +1,5 @@
+// Script para manejar el enlace de cierre
+document.querySelector('.btn-cerrar').addEventListener('click', function (event) {
+    event.preventDefault();
+    window.location.href = 'index.html#suscripcion'; // Navegar a la sección de suscripción
+});
